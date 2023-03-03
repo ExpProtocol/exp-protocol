d71bb1748fe5e31c3db501bef8681e42428c9cab
Kazuma.I <51313744+Kazuma7@users.noreply.github.com>
2023-03-04 03:48:12 +0900

    Fix: 修正

---
+++ hooks/useLend.ts
@@ -0,0 +1,11 @@
+// import { useContractWrite, usePrepareContractWrite } from "wagmi";
+
+export const useLend = (contractAddress: string) => {};
+// 	const { config, error } = usePrepareContractWrite({
+// 		address: contractAddress,
+// 		abi: ERC721ABI,
+// 		functionName: "mint",
+// 	});
+// 	const { writeAsync: lend } = useContractWrite(config);
+// 	return { lend };
+// };
