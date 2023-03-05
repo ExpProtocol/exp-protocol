986e7ac296be2693e131081bc92529663eee71e3
Kazuma.I <51313744+Kazuma7@users.noreply.github.com>
2023-03-06 04:10:03 +0900

    Fix: headerのデザイン修正

--- components/atoms/Header.tsx
+++ components/atoms/Header.tsx
@@ -1,18 +1,30 @@
+import Image from "next/image";
 import Link from "next/link";
 import { CustomConnectButton } from "../molecules/CustomConnectButton";
 
 export default function Header() {
-	return (
-		<nav className="flex items-center justify-between text-gray-800 h-[62px] bg-white px-5">
-			<Link href="/" className="font-semibold">
-				Lime
-			</Link>
-			<div className="flex justify-end items-center gap-6">
-				<Link href="/mypage" className="text-[#3EA8FF] font-bold text-sm">
-					My Page
-				</Link>
-				<CustomConnectButton />
-			</div>
-		</nav>
-	);
+    return (
+        <nav className="flex items-center justify-between text-gray-800 h-[62px] bg-white px-5">
+            <Link href="/" className="font-semibold">
+                <div className="relative h-[18px]  w-[200px]">
+                    <Image
+                        src={"/image.png"}
+                        fill
+                        style={{ objectFit: "cover" }}
+                        className="rounded-t-xl"
+                        alt=""
+                    />
+                </div>
+            </Link>
+            <div className="flex justify-end items-center gap-6">
+                <Link
+                    href="/mypage"
+                    className="text-[#3EA8FF] font-bold text-sm"
+                >
+                    My Page
+                </Link>
+                <CustomConnectButton />
+            </div>
+        </nav>
+    );
 }
